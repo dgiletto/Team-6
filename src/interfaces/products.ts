e4edea606d8600272bc3b89b9904a5406cb7099e
Rana <rantun@udel.edu>
2023-04-10 17:18:38 -0400

    Created file for "Product" class

---
+++ src/interfaces/products.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    name: string; // Name of item
+    image: string; // Path of image
+    price: number; // Cost of item
+    quantity: number; // # item in stock
+    in_stock: boolean; // If item in stock
+}
