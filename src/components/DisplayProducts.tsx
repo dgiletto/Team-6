f7c4c905eeb6255a83abef202d54361d787e99dd
Igneyy <igney64@gmail.com>
2023-04-29 00:16:19 -0400

    images display

--- src/components/DisplayProducts.tsx
+++ src/components/DisplayProducts.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 // import { Button, Row, Col } from "react-bootstrap";
 import products from "../data/products.json";
+import { images } from "../images";
 
 export function DisplayProducts(): JSX.Element {
     return (
@@ -11,7 +12,10 @@ export function DisplayProducts(): JSX.Element {
             <Row md={2} xs={1} lg={3} className="g-3">
                 {products.map((product) => (
                     <Col key={product.name}>
-                        <ProductCards {...product} />
+                        <ProductCards
+                            {...product}
+                            image={images[product.image]}
+                        />
                     </Col>
                 ))}
             </Row>
