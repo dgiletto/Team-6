95ea70e43d3bd08e655f773dba96902cb241ece3
kroth353 <121525795+kroth353@users.noreply.github.com>
2023-05-01 18:35:05 -0400

    Revert "done with cart?"

--- src/hooks/useProducts.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import productsContext from "../context/productsProvider";
-import { useProductsContextType } from "../context/productsProvider";
-
-const useProducts = (): useProductsContextType => {
-    return useContext(productsContext);
-};
-export default useProducts;
