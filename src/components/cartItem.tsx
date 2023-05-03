90711aa046009f01bddcc28a75b31370307e9eb6
Igneyy <igney64@gmail.com>
2023-05-03 17:40:47 -0400

    images load in side view of the cart, buttons will add and subtract on that cart sideview

--- src/components/cartItem.tsx
+++ src/components/cartItem.tsx
@@ -3,6 +3,7 @@ import { useShoppingCart } from "../context/shoppingCartContext";
 import products from "../data/products.json";
 import React from "react";
 import { formatCards } from "../extras/formatMoney";
+import { images } from "../images";
 
 type cartItemProps = {
     name: string;
@@ -11,6 +12,8 @@ type cartItemProps = {
 
 export function CartItem({ name, quantity }: cartItemProps) {
     const { removeFromCart } = useShoppingCart();
+    const { increaseCartQty } = useShoppingCart();
+    const { decreaseCartQty } = useShoppingCart();
     const item = products.find((i) => i.name == name);
 
     if (item == null) {
@@ -23,7 +26,7 @@ export function CartItem({ name, quantity }: cartItemProps) {
             className="d-flex align-items-center"
         >
             <img
-                src={item.image}
+                src={images[item.image]}
                 style={{ width: "125px", height: "75px", objectFit: "cover" }}
             />
             <div className="me-auto">
@@ -37,6 +40,29 @@ export function CartItem({ name, quantity }: cartItemProps) {
                     {formatCards(item.price)}
                 </div>
                 <div>{formatCards(item.price * quantity)}</div>
+                <div
+                    className="d-flex align-items-center justify-content-left"
+                    style={{ gap: ".5rem" }}
+                >
+                    <Button
+                        className="w-25"
+                        style={{ backgroundColor: "#829fda" }}
+                        onClick={() =>
+                            quantity > 1
+                                ? decreaseCartQty(name)
+                                : removeFromCart(item.name)
+                        }
+                    >
+                        -
+                    </Button>
+                    <Button
+                        className="w-25"
+                        style={{ backgroundColor: "#829fda" }}
+                        onClick={() => increaseCartQty(name)}
+                    >
+                        +
+                    </Button>
+                </div>
                 <Button
                     variant="outline-danger"
                     size="sm"
