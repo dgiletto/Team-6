20ac543d341984101c27f7030a6cd45c80ef64f0
Dylan Giletto <dgiletto@udel.edu>
2023-05-11 19:08:08 -0400

    Checkout Button

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,12 @@
-import { Offcanvas, Stack, Button, Modal } from "react-bootstrap";
+import {
+    Offcanvas,
+    Stack,
+    Button,
+    Modal,
+    Form,
+    Row,
+    Col
+} from "react-bootstrap";
 import React from "react";
 import { useShoppingCart } from "../context/shoppingCartContext";
 import { CartItem } from "./cartItem";
@@ -14,6 +22,11 @@ export function Cart({ isOpen }: CartProps) {
     const [show, setShow] = React.useState(false);
     const handleClose = () => setShow(false);
     const handleOpen = () => setShow(true);
+    const showButton =
+        cartItems.reduce((total, cartItem) => {
+            const item = products.find((i) => i.name == cartItem.name);
+            return total + (item?.price || 0) * cartItem.quantity;
+        }, 0) !== 0;
     return (
         <Offcanvas show={isOpen} onHide={closeCart} placement="end">
             <Offcanvas.Header closeButton>
@@ -38,11 +51,71 @@ export function Cart({ isOpen }: CartProps) {
                             }, 0)
                         )}
                     </div>
-                    <Button onClick={handleOpen}>Checkout</Button>
+                    {showButton && (
+                        <Button onClick={handleOpen}>Checkout</Button>
+                    )}
                     <Modal show={show} onHide={handleClose}>
                         <Modal.Header closeButton>
                             <Modal.Title>Checkout</Modal.Title>
                         </Modal.Header>
+                        <Modal.Body>
+                            Total:{" "}
+                            {formatCards(
+                                cartItems.reduce((total, cartItem) => {
+                                    const item = products.find(
+                                        (i) => i.name == cartItem.name
+                                    );
+                                    return (
+                                        total +
+                                        (item?.price || 0) * cartItem.quantity
+                                    );
+                                }, 0)
+                            )}
+                            <Form>
+                                <Form.Group id="cardholder name">
+                                    <Form.Label>Cardholder Name</Form.Label>
+                                    <Form.Control
+                                        type="cardholder name"
+                                        placeholder="John Doe"
+                                    />
+                                </Form.Group>
+                                <Form.Group id="card number">
+                                    <Form.Label>Card Number</Form.Label>
+                                    <Form.Control
+                                        type="card number"
+                                        placeholder="1234 5678 9101 1121"
+                                    />
+                                </Form.Group>
+                                <Row>
+                                    <Form.Group as={Col} id="cvv">
+                                        <Form.Label>CVV</Form.Label>
+                                        <Form.Control
+                                            type="cvv"
+                                            placeholder="123"
+                                        />
+                                    </Form.Group>
+                                    <Form.Group as={Col} id="expiration">
+                                        <Form.Label>Expiration Date</Form.Label>
+                                        <Form.Control
+                                            type="expiration"
+                                            placeholder="01/24"
+                                        />
+                                    </Form.Group>
+                                </Row>
+                                <Form.Group id="zip code">
+                                    <Form.Label>Zip Code</Form.Label>
+                                    <Form.Control type="zip code" />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Cancel
+                            </Button>
+                            <Button variant="primary" onClick={handleClose}>
+                                Sumbit
+                            </Button>
+                        </Modal.Footer>
                     </Modal>
                 </Stack>
             </Offcanvas.Body>
