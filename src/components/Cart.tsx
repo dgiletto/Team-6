3bd9d6cf960927bab7296578f52fdedcc09c476e
Dylan Giletto <dgiletto@udel.edu>
2023-05-11 17:33:28 -0400

    Checkout Button Produces Pop Up

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import { Offcanvas, Stack } from "react-bootstrap";
+import { Offcanvas, Stack, Button, Modal } from "react-bootstrap";
 import React from "react";
 import { useShoppingCart } from "../context/shoppingCartContext";
 import { CartItem } from "./cartItem";
@@ -11,6 +11,9 @@ type CartProps = {
 
 export function Cart({ isOpen }: CartProps) {
     const { closeCart, cartItems } = useShoppingCart();
+    const [show, setShow] = React.useState(false);
+    const handleClose = () => setShow(false);
+    const handleOpen = () => setShow(true);
     return (
         <Offcanvas show={isOpen} onHide={closeCart} placement="end">
             <Offcanvas.Header closeButton>
@@ -35,6 +38,12 @@ export function Cart({ isOpen }: CartProps) {
                             }, 0)
                         )}
                     </div>
+                    <Button onClick={handleOpen}>Checkout</Button>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Checkout</Modal.Title>
+                        </Modal.Header>
+                    </Modal>
                 </Stack>
             </Offcanvas.Body>
         </Offcanvas>
